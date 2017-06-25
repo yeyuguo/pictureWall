18f66de4e2870fa82fdc969460ed2dc42eee06c6
yeyuguo <346243440@qq.com>
2017-06-26 01:44:35 +0800

    delete something

--- src/index.js
+++ src/index.js
@@ -1,12 +1,8 @@
 import 'core-js/fn/object/assign';
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import App from './components/Main';
+import App from './components/Main';
 
-// Render the main component into the dom
-// ReactDOM.render(<App />, document.getElementById('app'));
 
-import PicApp from './components/Picture'
-ReactDOM.render(<PicApp />, document.getElementById('app'));
-
+ReactDOM.render(<App />, document.getElementById('app'));
 
